77b5689790156c1d5d7df4603f91c315e3dcd7ad
Alfred Mtambalika <karanlicka@gmail.com>
2026-02-17 17:58:56 +0530

    Implementing Auto scroll feature

--- src/ChatBot.jsx
+++ src/ChatBot.jsx
@@ -1,12 +1,19 @@
 
+import { useEffect, useRef } from "react";
 import ChatMessage from "./ChatMessage";
 import './Chatbot.css'
 
 
 function Chatbot({ messages }){
+  const chatMessagesRef = useRef(null);
+   useEffect(()=>{
+  const containerElem =  chatMessagesRef.current;
+  if(containerElem){
+     containerElem.scrollTop = containerElem.scrollHeight;
+  }
+   },[messages]);
    
-   
-  return(<div className="chat-messages-container">
+  return(<div className="chat-messages-container" ref={chatMessagesRef}>
    {
          (messages.map((chatMessage)=>{
           return(
