7f88610bb3769c74f4419b4296e54a93d6476662
Alfred Mtambalika <karanlicka@gmail.com>
2026-03-07 12:52:42 +0530

    Proper folder structure

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  root: 'frontend',
   base: "/Chatbot/",
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "http://localhost:5001",
+      "/api": "http://localhost:5000",
     },
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
   },
 })
